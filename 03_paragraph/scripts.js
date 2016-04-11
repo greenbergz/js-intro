6cffbe4eb75c8dca6fbdea37c6231c4c89325fde
greenbergz <zachgreenberg@zachs-air.dhcp.wustl.edu>
2016-04-11 18:41:19 -0500

    added stuff

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,6 @@
+document.write("<h1>TITLE</h1>");
+document.write("<p>I'm practicing 'debugging'.I'm practicing 'debugging'. I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.</p>");
+
+document.write("<p>I'm practicing 'debugging'.I'm practicing 'debugging'. I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.</p>");
+
+document.write("<p>I'm practicing 'debugging'.I'm practicing 'debugging'. I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.I'm practicing 'debugging'.</p>");
